2e52a4821ac929bdb9e965a3ce198d827d00f628
Wellisson Lima <wellissonboanerges@gmail.com>
2025-03-30 19:14:47 -0300

    Correção de erro de sintáxi no arquivo de solicitação

--- models/Solicitacao.js
+++ models/Solicitacao.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const SolicitacaoSchema = new mongoose.Schema({
     fabrica: { type: String, required: true },
     quantidade: { type: Number, required: true },
-    status: { Type: String, enum: ["pendente", "atendido"], default: "pendente" },
+    status: { type: String, enum: ["pendente", "atendido"], default: "pendente" },
     dataCriacao: { type: Date, default: Date.now },
 });
 
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const userRoutes = require("./routes/userRoutes");
+const solicitacaoRoutes = require("./routes/solicitacaoRoutes");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
@@ -16,6 +19,9 @@ mongoose
 //configuração básica do servidor
 app.use(cors());
 app.use(express.json()); // Permite receber JSON no corpo das requisições
+
+app.use("/api/usuarios", userRoutes);
+app.use("/api/solicitacoes", solicitacaoRoutes);
 
 app.get("/", (req, res) => {
     res.send("API de Solicitação de Caixas funcionando!");
